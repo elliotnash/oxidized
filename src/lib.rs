b5490d091442b32a72dea7e43bf697eab733036e
Elliot Nash <elliotnash@gmail.com>
2021-04-18 13:36:41 -0700

    remove unneeded inports

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,7 @@
-use tokio::{
-    sync::RwLock,
-    net::TcpStream,
-    time::{sleep, Duration}
-};
-use tokio_native_tls::TlsStream;
 use tracing::{info, debug};
-use reqwest::StatusCode;
-use async_tungstenite::{
-    WebSocketStream, 
-    stream::Stream, 
-    tokio::TokioAdapter, 
-    tungstenite::{
-        Message, 
-        handshake::client::Request
-    }
-};
-use futures::{prelude::*, stream::{SplitSink, SplitStream}};
 
 mod http;
-use http::{HttpClient, models::connection::Credentials};
+use http::HttpClient;
 
 const BASE_URL: &str = "https://www.guilded.gg/api";
 const WS_URL: &str = "wss://api.guilded.gg/socket.io/?jwt=undefined&EIO=3&transport=websocket";
