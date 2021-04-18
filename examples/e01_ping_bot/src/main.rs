2909e9c4c380660278c2d1155d34773aa25b665a
Elliot Nash <elliotnash@gmail.com>
2021-04-18 00:51:35 -0700

    use client struct instead of lazy_static

--- examples/e01_ping_bot/src/main.rs
+++ examples/e01_ping_bot/src/main.rs
@@ -4,5 +4,6 @@ async fn main() {
     let email = std::env::var("EMAIL").unwrap();
     let password = std::env::var("PASSWORD").unwrap();
     tracing_subscriber::fmt::init();
-    oxidized::login(&email, &password).await;
+    let client = oxidized::Client::login(&email, &password).await;
+    client.connect().await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,41 +10,44 @@ pub struct Client {
     client: reqwest::Client
 }
 
-pub async fn login(email: &str, password: &str) {
+impl Client {
 
-    let client = Client{
-        client: reqwest::ClientBuilder::new()
-            .cookie_store(true).build().unwrap()
-    };
+    pub async fn login(email: &str, password: &str) -> Self {
+        let client = Client{
+            client: reqwest::ClientBuilder::new()
+                .cookie_store(true).build().unwrap()
+        };
 
-    let result = client.client.post(format!("{}/login", BASE_URL))
-        .json(&Credentials{email: email.to_string(), password: password.to_string()}).send().await;
-    
-    match result {
-        Ok(response) => {
-            match response.status() {
-                StatusCode::OK => {
-                    info!("Logged in to guilded.gg!");
-                }
-                StatusCode::BAD_REQUEST => {
-                    panic!("Invalid login credentials");
-                }
-                _ => {}
-            };
-        }
-        Err(error) => {
-            debug!("Connection error:\n{}", error);
-            if let Some(status) = error.status() {
-                panic!("Connection to guilded returned: {}", status);
-            } else if error.is_timeout() {
-                panic!("Connection to guilded timed out");
-            } else {
-                panic!("Error connecting to guilded");
+        let result = client.client.post(format!("{}/login", BASE_URL))
+            .json(&Credentials{email: email.to_string(), password: password.to_string()}).send().await;
+        
+        match result {
+            Ok(response) => {
+                match response.status() {
+                    StatusCode::OK => {
+                        info!("Logged in to guilded.gg!");
+                    }
+                    StatusCode::BAD_REQUEST => {
+                        panic!("Invalid login credentials");
+                    }
+                    _ => {}
+                };
             }
-        }
-    };
-}
+            Err(error) => {
+                debug!("Connection error:\n{}", error);
+                if let Some(status) = error.status() {
+                    panic!("Connection to guilded returned: {}", status);
+                } else if error.is_timeout() {
+                    panic!("Connection to guilded timed out");
+                } else {
+                    panic!("Error connecting to guilded");
+                }
+            }
+        };
+        client
+    }
 
-pub async fn connect() {
-
+    pub async fn connect(&self) {
+        info!("Connected to guilded.gg");
+    }
 }
