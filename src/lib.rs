236a5a1607c1eb62c81a8b331f17462b9cdefd51
Elliot <elliotnash@gmail.com>
2021-04-24 17:39:35 -0700

    update hyperlinks, add code examples in docs

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@
 //! features = ["macros", "rt-multi-thread"]
 //! ```
 //! ### Basic Usage
-//! Create a new [`client::ClientBuilder`] to configure your client.
+//! Create a new [`ClientBuilder`](`client::ClientBuilder`) to configure your client.
 //! Loging in is preformed with user credentials (email password), as Guilded's bot api
 //! has not been released yet. We suggest creating a new account for your bot to 
 //! run under, and not using your main account.
@@ -45,8 +45,28 @@
 //! ### Event Handling
 //! If you are familliar with Serenity, you'll notice that events in Oxidized are
 //! handled in a very similar way. To recieve events, you'll need to pass a struct
-//! to your ClientBuilder that implements [`event::EventHandler`].
-
+//! to your [`ClientBuilder`](`client::ClientBuilder`) that implements 
+//! [`EventHandler`](`event::EventHandler`).
+//! ```rust
+//! struct Events;
+//! #[async_trait]
+//! impl EventHandler for Events {
+//!     async fn on_message(&self, event: ChatMessageCreated) {
+//!         // fired when a message is recieved
+//!     }
+//! }
+//! 
+//! #[tokio::main]
+//! async fn main() {
+//!     ClientBuilder::new()
+//!         .credentials("email", "password")
+//!         .event_handler(Events)
+//!         .login().await
+//!         .expect("Failed to create client")
+//!         .run().await;
+//! }
+//! ```
+//! To see the full list of events, look at [`event::EventHandler`]
 mod http;
 pub mod models;
 pub mod error;
